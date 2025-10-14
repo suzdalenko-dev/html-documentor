1b16f8c2e7388e66f2693397bb4470b07b39ff09
suzdalenko.suzdalenko@gmail.com <suzdalenko.suzdalenko@gmail.com>
2025-10-14 19:09:57 +0200

    su

--- assets/views/dashboard.js
+++ assets/views/dashboard.js
@@ -1,4 +1,5 @@
 
 async function dashboardInit(){
-    
+    document.title = 'Dashboard Documentor';
+    document.getElementById('slugTitle').innerHTML = '<h2 class="text-xl font-semibold mb-4">Documentor</h2>';
 }
